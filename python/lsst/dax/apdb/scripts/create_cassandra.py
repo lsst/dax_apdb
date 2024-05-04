ce078d51f8fe0ae2c246bafbf0e523f3c42e200d
Andy Salnikov <salnikov@slac.stanford.edu>
2024-05-03 17:41:07 -0700

    Introduce a way to specify table options when creating tables.
    `create-cassandra` CLI adds `--table-options` option pointing to a YAML file with
    table options.

--- python/lsst/dax/apdb/scripts/create_cassandra.py
+++ python/lsst/dax/apdb/scripts/create_cassandra.py
@@ -23,12 +23,18 @@ from __future__ import annotations
 
 __all__ = ["create_cassandra"]
 
+import io
 from typing import Any
 
-from ..cassandra import ApdbCassandra
+import yaml
+from lsst.resources import ResourcePath
+
+from ..cassandra import ApdbCassandra, CreateTableOptions
 
 
-def create_cassandra(output_config: str, ra_dec_columns: str | None, **kwargs: Any) -> None:
+def create_cassandra(
+    output_config: str, ra_dec_columns: str | None, table_options: str | None = None, **kwargs: Any
+) -> None:
     """Create new APDB instance in Cassandra cluster.
 
     Parameters
@@ -37,12 +43,28 @@ def create_cassandra(output_config: str, ra_dec_columns: str | None, **kwargs: A
         Name of the file to write APDB configuration.
     ra_dec_columns : `str` or `None`
         Comma-separated list of names for ra/dec columns in DiaObject table.
+    table_options : `str`, optional
+        Path or URI of the YAML file with table options.
     **kwargs
         Keyword arguments passed to `ApdbCassandra.init_database` method.
     """
+    options = _read_table_options(table_options)
     ra_dec_list: list[str] | None = None
     if ra_dec_columns:
         ra_dec_list = ra_dec_columns.split(",")
     kwargs["hosts"] = kwargs.pop("host")
-    config = ApdbCassandra.init_database(ra_dec_columns=ra_dec_list, **kwargs)
+    config = ApdbCassandra.init_database(ra_dec_columns=ra_dec_list, table_options=options, **kwargs)
     config.save(output_config)
+
+
+def _read_table_options(table_options: str | None = None) -> CreateTableOptions | None:
+    """Read contents of the table options file."""
+    if not table_options:
+        return None
+
+    res = ResourcePath(table_options)
+    content = res.read()
+    stream = io.BytesIO(content)
+    if model_data := yaml.load(stream, Loader=yaml.SafeLoader):
+        return CreateTableOptions.model_validate(model_data)
+    return None
